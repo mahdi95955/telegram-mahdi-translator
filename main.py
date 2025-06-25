bd8dad3c2ef41cd753669de243c77d75ed7436ce
mahdi95955 <azizimahdi948@gmail.com>
2025-06-25 20:12:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-from googletrans import Translator
-
-translator = Translator()
+from deep_translator import GoogleTranslator
 
 async def translate_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.text:
-        try:
-            translated = translator.translate(update.message.text, src='ar', dest='fa')
-            await update.message.reply_text(f"📤 ترجمه:
-{translated.text}")
-        except Exception as e:
-            await update.message.reply_text("❌ خطا در ترجمه.")
-    else:
-        await update.message.reply_text("❗ فقط پیام متنی قابل ترجمه است.")
+    try:
+        original_text = update.message.text
+        translated = GoogleTranslator(source='ar', target='fa').translate(original_text)
+        await update.message.reply_text(f"📤 ترجمه:\n{translated}")
+    except Exception as e:
+        await update.message.reply_text("❌ خطا در ترجمه.")
 
 app = ApplicationBuilder().token("7731294839:AAHv7Rt78_Jbxs6wlqv_SPGhFgmIL5mLJxA").build()
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, translate_message))
